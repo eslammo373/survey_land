13055dac34d9bd5b92f593ad8e55a63d9bc75851
eslammo373 <ee.eslam344@gmail.com>
2025-07-26 18:06:46 +0300

    seven commit

--- src/components/common/CardCommon.jsx
+++ src/components/common/CardCommon.jsx
@@ -4,7 +4,7 @@ import {
     CardFooter,
     CardHeader,
     CardTitle,
-} from "@/components/ui/card"
+} from "@/components/UI/card"
 import Btn from "./Btn"
 
 export default function CardCommon({ items }) {
